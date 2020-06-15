cd382763d81b5b005c496b2ade1dd82b3b6b1d04
parasol <619372857@qq.com>
2020-06-15 14:56:09 +0800

    使用协程下载文件

--- app/Service/PhotoService.php
+++ app/Service/PhotoService.php
@@ -8,30 +8,34 @@ use Hyperf\Guzzle\HandlerStackFactory;
 use GuzzleHttp\Client;
 use Swoole\Coroutine\Channel;
 
-//use Swoole\Coroutine\Http\Client;
+use Swoole\Coroutine\Http\Client as swooleClient;
 
 class PhotoService
 {
     private $client;
+    private $photoURL;
 
-    public function __construct()
+    public function __construct($photoURL)
     {
+        $this->photoURL = $photoURL;
         $this->getGuzzle();  //获取对象
 
     }
 
+    /**
+     * 测试使用Guzzle下载图片
+     */
     public function downPhoto()
     {
 
-
-        $chan = new Channel(1);
         $time1 = microtime(true);
-        $photoUrl = 'http://og9j6rxfr.bkt.clouddn.com/5039920180725114926214.jpg';
+        $chan = new Channel(1);
+        $photoUrl = $this->photoURL;
         go(function () use ($photoUrl, $chan) {
             $path = '';
             $photoData = $this->client->get($photoUrl);
             if ($photoData->getStatusCode() == 200) {  //表示请求成功
-                $path = BASE_PATH . "/runtime/photo/" . uniqid() . '.png';
+                $path = BASE_PATH . "/runtime/photo/" . uniqid() . 'Guzzle.png';
                 $fp = fopen($path, "a+");
                 fwrite($fp, $photoData->getBody()->getContents());
                 fclose($fp);
@@ -40,12 +44,51 @@ class PhotoService
                 'path' => $path
             ));
         });
-        $data = $chan->pop();  //获取协程的值
-        var_dump($data);
-        echo microtime(true) - $time1 . PHP_EOL;
+        $chan->pop();  //获取协程的值
+        $time2 = microtime(true) - $time1;
+        echo 'Guzzle: ' . $time2 . PHP_EOL;
+    }
+
+    public function downSwoolePhoto()
+    {
+        $time1 = microtime(true);
+        $chan = new Channel(1);
+        $photoUrl = $this->photoURL;
+        go(function () use ($photoUrl, $chan) {
+            $host = parse_url($photoUrl)['host'];
+            $client = new swooleClient($host, 80);
+            $client->set(['timeout' => -1]);
+            $path = BASE_PATH . "/runtime/photo/" . uniqid() . 'Swoole.png';
+            $client->download(parse_url($photoUrl)['path'], $path);
+            $chan->push(array(
+                'path' => $path
+            ));
+        });
+        $chan->pop();  //获取协程的值
+        $time2 = microtime(true) - $time1;
+        echo 'Swoole: ' . $time2 . PHP_EOL;
+    }
+
+    public function fileGet()
+    {
+        $time1 = microtime(true);
+        $chan = new Channel(1);
+        $photoUrl = $this->photoURL;
+        go(function () use ($photoUrl, $chan) {
+            $path = BASE_PATH . "/runtime/photo/" . uniqid() . 'fileGet.png';
+            $resData = file_get_contents($photoUrl);
+            file_put_contents($path, $resData);
+            $chan->push(array(
+                'path' => $path
+            ));
+        });
+        $chan->pop();  //获取协程的值
+        $time2 = microtime(true) - $time1;
+        echo 'fileGet: ' . $time2 . PHP_EOL;
 
 
     }
+
 
     public function getGuzzle()
     {
