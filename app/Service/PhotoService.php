3266dca1d75d58bec49ffa8d108d9ad7ea079465
parasol <619372857@qq.com>
2020-06-15 10:48:05 +0800

    协程下载文件

--- app/Service/PhotoService.php
+++ app/Service/PhotoService.php
@@ -6,6 +6,7 @@ namespace App\Service;
 
 use Hyperf\Guzzle\HandlerStackFactory;
 use GuzzleHttp\Client;
+use Swoole\Coroutine\Channel;
 
 //use Swoole\Coroutine\Http\Client;
 
@@ -21,19 +22,28 @@ class PhotoService
 
     public function downPhoto()
     {
-        $photoUrl = 'http://og9j6rxfr.bkt.clouddn.com/5039920180725114926214.jpg';
 
-        $photoData = $this->client->get($photoUrl);
-//        var_dump($photoData->getStatusCode );
-        if ($photoData->getStatusCode() == 200) {  //表示请求成功
-            $path = BASE_PATH . "/runtime/photo";
-            if (!file_exists($path)) {
-                mkdir($path);
+
+        $chan = new Channel(1);
+        $time1 = microtime(true);
+        $photoUrl = 'http://og9j6rxfr.bkt.clouddn.com/5039920180725114926214.jpg';
+        go(function () use ($photoUrl, $chan) {
+            $path = '';
+            $photoData = $this->client->get($photoUrl);
+            if ($photoData->getStatusCode() == 200) {  //表示请求成功
+                $path = BASE_PATH . "/runtime/photo/" . uniqid() . '.png';
+//            var_dump($path);
+                $fp = fopen($path, "a+");
+                fwrite($fp, $photoData->getBody()->getContents());
+                fclose($fp);
             }
-            $fp = fopen($path, "a+");
-            fwrite($fp, $photoData->getBody()->getContents());
-            fclose($fp);
-        }
+            $chan->push(array(
+                'path' => $path
+            ));
+        });
+        $data = $chan->pop();  //获取协程的值
+        var_dump($data);
+        echo microtime(true) - $time1 . PHP_EOL;
 
 
     }
